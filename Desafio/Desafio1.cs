85fb66e49b32fd6eb0af37683245a23b57a7fea5
LuisAlan89 <luisalan.lopez@cetys.edu.mx>
2022-09-05 21:04:49 -0700

    Actividad #1

--- Desafio/Desafio1.cs
+++
@@ -1,2 +0,0 @@
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
--- Solucion/Solucion1.cs
+++
@@ -1,5 +0,0 @@
-Console.WriteLine("This is the first line.");
-
-Console.Write("This is ");
-Console.Write("the second ");
-Console.Write("line.");
